a02b03795cb3eb4eaba16f9b7c41084a718e9337
Julia Alexeeva <julia4406@gmail.com>
2024-12-17 17:41:44 +0200

    Fixed style fails

--- app/errors.py
+++ app/errors.py
@@ -1,17 +1,9 @@
-"""
-different messages processing
-was made in educational purposes
-"""
-
-
 class NotWearingMaskError(Exception):
-    def __init__(self, message: str = "NotWearingMaskError()") -> None:
-        super().__init__(message)
+    pass
 
 
 class VaccineError(Exception):
-    def __str__(self) -> str:
-        return f"{self.__class__.__name__}()"
+    pass
 
 
 class NotVaccinatedError(VaccineError):
